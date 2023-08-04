2e85c7088b1f86b870912c7b9b856aaea3253c8d
Mecopi <mecopilive@gmail.com>
2023-08-04 14:34:12 +0200

    feat(map): add base of map class

---
+++ PathFounding/core/map/Map.cs
@@ -0,0 +1,27 @@
+﻿namespace PathFounding.core.map
+{
+    public class Map
+    {
+        private int _Width; // Mesured per road
+        private int _Height; // Mesured per road
+        public Map()
+        {
+
+        }
+
+
+        #region Getters
+        public int Width
+        {
+            get => _Width;
+            set => _Width = _Width > 0 ? _Width = value : 10;   
+        }
+
+        public int Height
+        {
+            get => _Height;
+            set => _Height = _Height > 0 ? _Height = value : 10;
+        }
+		#endregion
+	}
+}
